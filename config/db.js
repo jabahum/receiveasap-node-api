14350ee78ba7961d94220ad54c9d0eada6c3b23a
jabahum-coder <jabajnan8@gmail.com>
2021-02-01 07:47:50 +0300

    Edited
    Signed-off-by: jabahum-coder <jabajnan8@gmail.com>

--- config/db.js
+++ config/db.js
@@ -2,28 +2,11 @@
 const mongoose = require('mongoose');
 //mongodb+srv://admin:<password>@test.oamsg.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-        // development: {
-        //     connection_string:"mongodb://localhost:27017/receiveasap_api",
-        //     params: {
-        //         useNewUrlParser: true,
-        //         useUnifiedTopology: true,
-        //         useCreateIndex: true
-        //     }
-        // },
-        //Connection to Mongo Atlas
-        //Used by the Kumusoft Online test platform (https://efris.kumusoft.com)
-        mongo_atlas: {
-            connection_string: 'mongodb+srv://admin:password@test.oamsg.gcp.mongodb.net/test?retryWrites=true&w=majority',
-            params: {
-                user: "admin",
-                pass: "password",
-                dbName: "receiveasap_api",
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true
-            }
-        }
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
     });
 
     console.log(`🔖 DB Connected successfully to: ${conn.connection.host}`);
